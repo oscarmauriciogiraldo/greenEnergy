3fb87f9ccc63e3190c6c5dd496bc8d8bab223e6b
OscarGiraldo <oscar.giraldo@linktic.com>
2025-05-19 11:27:46 -0500

    init project ecoEnergy

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/sections/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Hero></Hero>
+    </div>
+  )
+} 
+
+export default App
