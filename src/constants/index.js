f553c4195a23e401a96077c0ec9df0cf806cc6bd
OscarGiraldo <oscar.giraldo@linktic.com>
2025-05-19 20:53:35 -0500

    header and navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,9 @@
-
+export const NavbarMenu = [
+    {id: 1, title: 'Home',  path: '/',},
+    {id: 2, title: 'Energias renovables',  path: '/',},
+    {id: 3, title: 'Nuestro Proposito',  path: '/',},
+    {id: 4, title: 'Proyectos',  path: '/',},
+]
 export const wordsEnergy = [
     {text: 'Sostenibles', imgPath: '/images/energy1.svg'},
     {text: 'Limpias', imgPath: '/images/energy2.svg'},
@@ -6,4 +11,8 @@ export const wordsEnergy = [
     {text: 'Del Futuro', imgPath: '/images/energy1.svg'},
     {text: 'Infinitas', imgPath: '/images/energy2.svg'},
     {text: 'Que cuidan el Planeta', imgPath: '/images/eolic-energy.svg'},
+]
+
+export const menuTabs = [
+    {}
 ]
